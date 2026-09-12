77a367008f6cd207753671bbba5e56fd22d608cb
otaaaaaaaaaaaa <donguriota@gmail.com>
2026-09-12 12:11:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI, Request
+from linebot import LineBotApi, WebhookParser
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = FastAPI()
+
+CHANNEL_ACCESS_TOKEN = "あなたのアクセストークン"
+CHANNEL_SECRET = "あなたのシークレット"
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(CHANNEL_SECRET)
 
 @app.post("/callback")
 async def callback(request: Request):
     body = await request.body()
-    print("LINEから受信:", body)
+    signature = request.headers.get("X-Line-Signature")
+
+    events = parser.parse(body.decode(), signature)
+
+    for event in events:
+        if isinstance(event, MessageEvent) and isinstance(event.message, TextMessage):
+            reply_text = f"やっほー！『{event.message.text}』って送った？"
+
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_text)
+            )
+
     return "OK"
