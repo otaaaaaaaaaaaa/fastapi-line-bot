05e481adc5eccb1b57f8fb375d040fa73bb08be5
otaaaaaaaaaaaa <donguriota@gmail.com>
2026-09-12 19:08:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(CHANNEL_SECRET)
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 
 @app.get("/")
